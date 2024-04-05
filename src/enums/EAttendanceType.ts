f1a7c8147eaff199b130ae388c19513f4eca555c
olneha <olneha@ttu.ee>
2024-04-05 17:54:53 +0300

    3-EventsPage: enum event type

---
+++ src/enums/EAttendanceType.ts
@@ -0,0 +1,8 @@
+export enum EAttendanceType {
+    Other = "Other",
+    Trip = "Trip",
+    FunEvent = "Motivational event",
+    MonthlyMeeting = "Monthly meeting",
+    ExceptionalMeeting = "Exceptional meeting",
+    Meeting = "Meeting"
+}
