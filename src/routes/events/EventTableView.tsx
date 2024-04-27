d253930dd19d591819b27b7f8981e44dead57984
olneha <olneha@ttu.ee>
2024-04-27 15:24:54 +0300

    3-Events: table adjustments

--- src/routes/events/EventTableView.tsx
+++ src/routes/events/EventTableView.tsx
@@ -79,9 +79,8 @@ const EventTableView: React.FC<EventTableViewProps> = ({
             <Table striped={false} bordered hover className="mt-4">
                 <thead>
                 <tr>
-                    <th>Title</th>
-                    <th>Date</th>
-                    <th>Comment</th>
+                    <th></th>
+                    <th>Event</th>
                     <th scope="col" style={{cursor: 'pointer'}} onClick={toggleSort}>Helpers
                         needed {sortAscending ? '↑' : '↓'}</th>
                     <th>Helpers</th>
@@ -93,34 +92,28 @@ const EventTableView: React.FC<EventTableViewProps> = ({
                 {filteredEvents.map(event => (
 
                     <tr key={event.id}>
+                        <td> {event.eventDate ? format(new Date(event.eventDate), 'dd MMMM yyyy HH:mm') : 'N/A'}</td>
                         <td><OverlayTrigger
                             placement="top"
                             overlay={
                                 <Tooltip id="info-tooltip"> {event.attendanceType} </Tooltip>}>
                             <i className="bi bi-info-circle"></i>
-                        </OverlayTrigger> {event.eventTitle} </td>
-                        <td>{event.eventDate ? format(new Date(event.eventDate), 'dd MMMM yyyy HH:mm') : 'N/A'} </td>
+                        </OverlayTrigger> {event.eventTitle}
+                            <br/><br/> {event.comment}
+                        </td>
 
-                        <td>{event.comment}</td>
                         <td>
-                            {memberNames[event.id] ? (
+                            {memberNames[event.id] && (
                                 <>
-                                    Registered: {memberNames[event.id].length} <br/>
-                                    Needed: {event.helpersNeeded} <br/>
-                                    Total: {event.helpersNeeded - memberNames[event.id]?.length ?? 0}
-                                </>
-                            ) : (
-                                <>
-                                    Registered: 0 <br/>
-                                    Needed: {event.helpersNeeded} <br/>
-                                    Total: {event.helpersNeeded}
+                                    <p className={"esn-orange-half"}>Needed: {event.helpersNeeded - memberNames[event.id]?.length ?? 0} </p>
+                                    Registered: {memberNames[event.id].length}/{event.helpersNeeded} <br/>
                                 </>
                             )}
                         </td>
                         <td>
                             {memberNames[event.id] && memberNames[event.id].length > 0 ?
                                 memberNames[event.id].map((name) => <div key={name}>{name}<br/></div>) :
-                                <Alert variant="secondary">Join now! 🚀</Alert>}
+                                <Alert variant="secondary">Be the first one to join! 🚀</Alert>}
                         </td>
                         <td>
                             {(event.helpersNeeded - (memberNames[event.id]?.length ?? 0)) > 0
@@ -143,7 +136,7 @@ const EventTableView: React.FC<EventTableViewProps> = ({
                             </div>
                         </td>
                     </tr>
-                    ))}
+                ))}
                 </tbody>
             </Table>
         </>
