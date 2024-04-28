69f36b6585250ea0a1065229d0ee65f1d72f8704
olneha <olneha@ttu.ee>
2024-04-28 14:32:50 +0300

    2-PointsPage separation of encouragement utility

---
+++ src/components/EncouragementUtility.ts
@@ -0,0 +1,11 @@
+class EncouragementUtility {
+    static expressionsOfEncouragement: string[] = [
+        "Time to make your first steps to become Erasmus Overlord 🌟",
+        "Psst, want to earn some points? 👀",
+        "Every journey begins with a single step. 👣 You can earn some points helping!",
+        "Check the events to earn your first point! 🎉",
+        "🍀 Lucky Newbie! Let's start earning points together!"
+    ];
+}
+
+export default EncouragementUtility;
