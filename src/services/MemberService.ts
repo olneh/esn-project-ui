fa0e2f88634970bbf42cb7375b04a53bb966ca4c
olneha <olneha@ttu.ee>
2024-04-29 12:35:30 +0300

    10-AdjustingLocation: member data

--- src/services/MemberService.ts
+++ src/services/MemberService.ts
@@ -1,27 +1,40 @@
-import {IRegisterData} from "../entities/registration/IRegisterData";
-import {ILoginData} from "../entities/registration/ILoginData";
 import {BaseService} from "./BaseService";
-import {Axios, AxiosError} from "axios";
-import {IJWTResponse} from "../entities/registration/IJWTResponse";
 import {IMember} from "../entities/IMember";
 
 
 export class MemberService extends BaseService {
     constructor() {
-        super('account/');
+        super('members/');
     }
 
     getAllMembers = async (): Promise<IMember[]> => {
         try {
-            const response = await this.axios.get<IMember[]>('allMembers');
-
-            // if (response.status === 200) {
-                return response.data;
-            // }
+            const response = await this.axios.get<IMember[]>('all');
+            return response.data;
         } catch (e) {
             console.log('error: ', (e as Error).message, e);
 
             return [];
+        }
+    }
+
+    getMemberById = async (id: string): Promise<IMember | null> => {
+        try {
+            const response = await this.axios.get<IMember>(`member/${id}`);
+            return response.data;
+        } catch (e) {
+            console.log('error: ', (e as Error).message, e);
+            return null;
+        }
+    }
+
+    updateMemberById = async (id: string, memberData: Partial<IMember>): Promise<IMember | null> => {
+        try {
+            const response = await this.axios.put<IMember>(`member/${id}`, memberData);
+            return response.data;
+        } catch (e) {
+            console.log('error: ', (e as Error).message, e);
+            return null;
         }
     }
 
