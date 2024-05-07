97d131af895ffadf5546ca0141f1f713614b3a3a
olneha <olneha@ttu.ee>
2024-05-07 23:44:55 +0300

    9-Feedbacks: sort visual change

--- src/routes/feedback/FeedbackManager.tsx
+++ src/routes/feedback/FeedbackManager.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
-import { EventService } from '../../services/EventService';
-import { FeedbackService } from '../../services/FeedbackService';
-import { IEvent } from '../../entities/IEvent';
-import { IFeedback } from '../../entities/IFeedback';
+import React, {useEffect, useState} from 'react';
+import {Container, Row} from 'react-bootstrap';
+import {EventService} from '../../services/EventService';
+import {FeedbackService} from '../../services/FeedbackService';
+import {IEvent} from '../../entities/IEvent';
+import {IFeedback} from '../../entities/IFeedback';
 import FeedbackModal from "./FeedbackForm";
 import EventFeedbackCard from "./EventFeedbackCard";
 
@@ -15,32 +15,35 @@ const FeedbackManager: React.FC = () => {
     const [selectedEvent, setSelectedEvent] = useState<IEvent | null>(null);
     const [feedbackText, setFeedbackText] = useState('');
 
-    useEffect(() => {
-        const fetchEvents = async () => {
-            try {
-                const eventsFromService = await eventService.getAllEvents();
-                if (Array.isArray(eventsFromService)) {
-                    const sortedEvents = eventsFromService.sort((a, b) => {
-                        const dateA = a.eventDate ? new Date(a.eventDate) : new Date(0);
-                        const dateB = b.eventDate ? new Date(b.eventDate) : new Date(0);
-                        const monthA = dateA.getMonth();
-                        const dayA = dateA.getDate();
-                        const monthB = dateB.getMonth();
-                        const dayB = dateB.getDate();
-                        return monthA === monthB ? dayA - dayB : monthA - monthB;
-                    });
+    function eventsSortedByDate(a: IEvent, b: IEvent) {
+        const dateA = a.eventDate ? new Date(a.eventDate) : new Date(0);
+        const dateB = b.eventDate ? new Date(b.eventDate) : new Date(0);
+        const monthA = dateA.getMonth();
+        const dayA = dateA.getDate();
+        const monthB = dateB.getMonth();
+        const dayB = dateB.getDate();
+        return monthA === monthB ? dayA - dayB : monthA - monthB;
+    }
 
-                    setEvents(sortedEvents);
-                } else {
-                    console.error('Event data is not an array');
+    useEffect(() => {
+            const fetchEvents = async () => {
+                try {
+                    const eventsFromService = await eventService.getAllEvents();
+                    if (Array.isArray(eventsFromService)) {
+                        const sortedEvents = eventsFromService.sort((a, b) => {
+                            return eventsSortedByDate(a, b);
+                        });
+                        setEvents(sortedEvents);
+                    } else {
+                        console.error('Event data is not an array');
+                        setEvents([]);
+                    }
+                } catch (error) {
+                    console.error('Error fetching events:', error);
                     setEvents([]);
                 }
-            } catch (error) {
-                console.error('Error fetching events:', error);
-                setEvents([]);
-            }
-        };
-        fetchEvents();
+            };
+            fetchEvents();
     }, []);
 
     const isEventPassed = (eventDate: Date) => new Date(eventDate) < new Date();
